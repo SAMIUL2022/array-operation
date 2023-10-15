893d05f96a9332116c0491e550c4dbe7af9fef30
SAMIUL2022 <sami35bd@gmail.com>
2023-10-15 18:23:23 +0600

    updatedt insert element

--- insert_element.c
+++ insert_element.c
@@ -3,28 +3,25 @@ int main ()
 {
 int n;
 scanf("%d",&n);
-int arr [n+1];
-
-for (int i = 0; i < n; i++)
+int arr[n+1];
+for (int i = 0; i <n ; i++)
 {
-    scanf("%d",&arr[i]);
+   scanf("%d",&arr[i]); 
 }
 
 int pos,val;
 scanf("%d %d",&pos,&val);
-for (int i = n; i >= pos+1; i--)
+
+for (int i =n; i >=pos+1 ; i--)
 {
     arr[i]=arr[i-1];
-    // printf("%d ",arr[n+1]);
 }
-arr[pos] =val;
-for (int i = 0; i < n+1; i++)
+arr[pos]=val;
+for (int i =0; i <=n ; i++)
 {
-    
-
-printf("%d ",arr[i]);
+    printf("%d ",arr[i]);  
 }
 
-     
+  
     return 0;
 }
